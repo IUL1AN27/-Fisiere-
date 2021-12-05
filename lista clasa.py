c5e64f038cb9c8512dd1afc04dfadeaa6397ae5b
IUL1AN27 <70662088+IUL1AN27@users.noreply.github.com>
2021-12-06 00:13:05 +0200

    Add files via upload

---
+++ lista clasa.py
@@ -0,0 +1,10 @@
+with open('c:/Users/Desktop/ListaClasei11A.txt', 'r') as f:
+    elevi=list(f)
+n=media=0
+print('Nume','Prenume','Nota',sep='\t')
+for i in elevi:
+    elev=i.split()
+    nota=int(elev[2])
+    n,media=n+1,media+nota
+    print(elev[0],elev[1],nota,sep='\t')
+print('Media celor', n, 'elevi este', sum(media)/float(n))
